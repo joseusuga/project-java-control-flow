de3cc13fbfddc1cb6af6c5ce43edaeebe956aba5
jose usuga <jose1028.m@gmail.com>
2025-05-10 17:39:13 -0500

    comprendiendo las Sentencias If-Else

---
+++ EdadPersonita.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EdadPersonita {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu edad: ");
+        int edad = scanner.nextInt();
+
+        if (edad < 18) {
+            System.out.println("Eres menor de edad.");
+        } else {
+            System.out.println("Eres mayor de edad.");
+        }
+    }
+}
+
