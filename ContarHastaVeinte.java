8ad54c769d7499e747719b9fcc0c798cf1612696
jose usuga <jose1028.m@gmail.com>
2025-05-10 18:18:39 -0500

    Explorando los Bucles While

---
+++ ContarHastaVeinte.java
@@ -0,0 +1,13 @@
+public class ContarHastaVeinte {
+    public static void main(String[] args) {
+        
+        int contador = 1;
+        
+        while (contador <= 20) {
+            System.out.println(contador); 
+            contador++;  
+        }
+
+        System.out.println("La cuenta ha terminado, esto cansado jefe.");
+    }
+}
