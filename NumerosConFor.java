3dcb3487960714eab58d55dde1837cf621c5686e
jose usuga <jose1028.m@gmail.com>
2025-05-10 18:00:02 -0500

    Trabajando con Bucles For

---
+++ NumerosConFor.java
@@ -0,0 +1,10 @@
+
+public class NumerosConFor {
+    public static void main(String[] args) {
+        System.out.println("Imprimiendo los números del 1 al 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Número: " + i);
+        }
+    }
+}
